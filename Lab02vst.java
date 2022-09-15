2a206fec19ee51fe064c50e9c6d209a62387ee95
BaileyMann <bailey.mann@vtsd.com>
2022-09-15 12:08:10 -0400

    My first lab

--- Lab02vst.java
+++ Lab02vst.java
@@ -1,6 +1,26 @@
-public class Lab02vst {
+// Lab02v80.java
+//The 80-point version of Lab 02 is an exercise in precisely copying a
+// provided program, followed by compiling the source code into bytecode
+// and executing the bytecode file.
 
-    public static void main(String args[]){
+public class Lab02vst
+{
+    public static void main(String [] args)
+    {
+        System.out.println();
+        System.out.println("Lab 02, 100 point Version");
+        System.out.println();
+        System.out.println();
+        System.out.print("Measure today's knowledge");
+        System.out.println("by yesterday's confusion.");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious\n");
+        System.out.println("# ##### #####     ##### ##### # #    # ##### #####");
+        System.out.println("# #   # #   #     #   # #   # # ##   #   #   #");
+        System.out.println("# #   # #   #     ##### #   # # # #  #   #   #####");
+        System.out.println("# #   # #   #     #     #   # # #  # #   #       #");
+        System.out.println("# ##### #####     #     ##### # #    #   #   #####");
+
 
 
     }
